67250e2ac5430be9bc5a701ddb2e0e748fd7a568
Aakash Shah <subs@appsparkler.com>
2022-03-14 21:59:52 +0530

    chore: setup check-in component

---
+++ src/components/CheckIn/index.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { CheckIn } from "./index";
+
+export default {
+  title: "Components/Check In",
+  component: CheckIn,
+};
+
+const Template: ComponentStory<typeof CheckIn> = (args) => (
+  <CheckIn {...args} />
+);
+
+export const example = Template.bind({});
+example.args = {};
---
+++ src/components/CheckIn/index.tsx
@@ -0,0 +1,3 @@
+export const CheckIn = () => {
+  return <div>Check In</div>;
+};
