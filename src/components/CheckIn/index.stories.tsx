a76d8f1a61e4849e13181fcf3cb68c995993a003
Aakash Shah <subs@appsparkler.com>
2022-03-14 22:01:49 +0530

    chore: removed un-used files

--- src/components/CheckIn/index.stories.tsx
+++ src/components/CheckIn/index.stories.tsx
@@ -5,7 +5,7 @@ import { CheckIn } from "./index";
 export default {
   title: "Components/Check In",
   component: CheckIn,
-};
+} as ComponentMeta<typeof CheckIn>;
 
 const Template: ComponentStory<typeof CheckIn> = (args) => (
   <CheckIn {...args} />
