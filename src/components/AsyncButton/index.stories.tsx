99206ce50e5c19ce061f34dffb7abc2fb64683aa
Aakash Shah <subs@appsparkler.com>
2022-03-17 12:01:16 +0530

    chore: async-button to new module

---
+++ src/components/AsyncButton/index.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { AsyncButton, AsyncButtonProps } from "./index";
+import random from "lodash/fp/random";
+import Box from "@mui/material/Box";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Components/Async Button",
+  component: AsyncButton,
+} as ComponentMeta<typeof AsyncButton>;
+
+const AsyncButtonTemplate: ComponentStory<typeof AsyncButton> = (args) => (
+  <Box sx={{ display: "flex", justifyContent: "center", marginY: 5 }}>
+    <AsyncButton {...args} />
+  </Box>
+);
+
+export const asyncButton = AsyncButtonTemplate.bind({});
+asyncButton.args = {
+  label: "Check In",
+  onClick: (...args) => {
+    action("onClick")(...args);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (random(1)(2) === 1) resolve("success");
+        else reject();
+      }, 600);
+    });
+  },
+  successMessage: "Prakash is checked in",
+  errorMessage: "Prakash Mishra not checked in",
+} as AsyncButtonProps;
---
+++ src/components/AsyncButton/index.tsx
@@ -0,0 +1,102 @@
+import Alert, { AlertColor } from "@mui/material/Alert";
+import CircularProgress from "@mui/material/CircularProgress";
+import Snackbar, { SnackbarProps } from "@mui/material/Snackbar";
+import Box from "@mui/material/Box";
+import React, { useCallback, useState } from "react";
+import Button from "@mui/material/Button";
+import random from "lodash/fp/random";
+import { ClickHandler } from "../../types";
+
+export type AsyncButtonProps = {
+  onClick: ClickHandler;
+  disabled?: boolean;
+  errorMessage: string;
+  successMessage: string;
+  label: string;
+};
+
+export const AsyncButton = ({
+  onClick = async () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (random(1)(2) === 1) resolve();
+        else reject("API failed");
+      }, 500);
+    });
+  },
+  disabled,
+  label = "Sign In",
+  successMessage = "Done!",
+  errorMessage = "Oops! Something went wrong!",
+}: AsyncButtonProps) => {
+  const [snackbar, setSnackbar] = useState<{
+    isOpen: boolean;
+    message: string;
+    severity: AlertColor;
+  }>({
+    isOpen: false,
+    message: "",
+    severity: "error",
+  });
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const handleClick = useCallback<ClickHandler>(
+    async (evt) => {
+      setIsProcessing(true);
+      try {
+        await onClick(evt);
+        setSnackbar({
+          isOpen: true,
+          message: successMessage,
+          severity: "success",
+        });
+      } catch (e) {
+        setSnackbar({
+          isOpen: true,
+          message: errorMessage,
+          severity: "error",
+        });
+      } finally {
+        setIsProcessing(false);
+      }
+    },
+    [errorMessage, onClick, successMessage]
+  );
+  const handleSnackbarClose = useCallback<SnackbarProps["onClose"]>(() => {
+    setSnackbar((prev) => ({
+      ...prev,
+      isOpen: false,
+    }));
+  }, []);
+
+  return (
+    <Box sx={{ position: "relative", display: "inline-block" }}>
+      <Button
+        variant="contained"
+        type="button"
+        onClick={handleClick}
+        disabled={disabled || Boolean(isProcessing)}
+      >
+        {label}
+      </Button>
+      {isProcessing && (
+        <CircularProgress
+          sx={{
+            position: "absolute",
+            left: "calc(50% - 10px)",
+            top: "calc(50% - 10px)",
+          }}
+          size={20}
+        />
+      )}
+      <Snackbar
+        open={snackbar.isOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert severity={snackbar.severity} sx={{ width: "100%" }}>
+          {snackbar.message}
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
+};
