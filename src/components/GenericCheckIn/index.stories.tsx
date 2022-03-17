fc92c8cec17f6d9e66e165c6edca7130fb0c1c66
Aakash Shah <subs@appsparkler.com>
2022-03-17 12:13:24 +0530

    chore: minor clean up

--- src/components/GenericCheckIn/index.stories.tsx
+++ src/components/GenericCheckIn/index.stories.tsx
@@ -1,22 +1,8 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import {
-  AsyncButton,
-  AsyncButtonProps,
-  GenericCheckIn,
-  GenericCheckInProps,
-} from "./index";
+import { GenericCheckIn, GenericCheckInProps } from "./index";
 import { random, uniqueId } from "lodash/fp";
 import { action } from "@storybook/addon-actions";
-import Button from "@mui/material/Button";
-import { ClickHandler } from "../SignedInUserCheckIn";
-import {
-  Alert,
-  AlertColor,
-  Box,
-  CircularProgress,
-  Snackbar,
-} from "@mui/material";
 
 export default {
   title: "Components/Generic Check In",
@@ -27,101 +13,8 @@ const Template: ComponentStory<typeof GenericCheckIn> = (args) => (
   <GenericCheckIn {...args} />
 );
 
-const label = "Sign In";
-
-export const SignInSignOutButton = ({
-  onClick = async () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (random(1)(2) === 1) resolve();
-        else reject("API failed");
-      }, 500);
-    });
-  },
-  disabled,
-  successMessage = "Done!",
-  errorMessage = "Oops! Something went wrong!",
-}: {
-  onClick: ClickHandler;
-  disabled?: boolean;
-  errorMessage: string;
-  successMessage: string;
-}) => {
-  const [snackbar, setSnackbar] = useState<{
-    isOpen: boolean;
-    message: string;
-    severity: AlertColor;
-  }>({
-    isOpen: false,
-    message: "",
-    severity: "error",
-  });
-  const [isProcessing, setIsProcessing] = useState<boolean>(false);
-  const handleClick = useCallback<ClickHandler>(
-    async (evt) => {
-      setIsProcessing(true);
-      try {
-        await onClick(evt);
-        setSnackbar({
-          isOpen: true,
-          message: successMessage,
-          severity: "success",
-        });
-      } catch (e) {
-        setSnackbar({
-          isOpen: true,
-          message: errorMessage,
-          severity: "error",
-        });
-      } finally {
-        setIsProcessing(false);
-      }
-    },
-    [errorMessage, onClick, successMessage]
-  );
-  const handleSnackbarClose = useCallback(() => {
-    setSnackbar({ isOpen: false, message: "", severity: undefined });
-  }, []);
-  return (
-    <Box sx={{ position: "relative", display: "inline-block" }}>
-      <Button
-        variant="contained"
-        type="button"
-        onClick={handleClick}
-        disabled={disabled || Boolean(isProcessing)}
-      >
-        {label}
-      </Button>
-      {isProcessing && (
-        <CircularProgress
-          sx={{
-            position: "absolute",
-            left: "calc(50% - 10px)",
-            top: "calc(50% - 10px)",
-          }}
-          size={20}
-        />
-      )}
-      <Snackbar
-        open={snackbar.isOpen}
-        autoHideDuration={6000}
-        onClose={handleSnackbarClose}
-      >
-        <Alert
-          severity={snackbar.severity}
-          sx={{ width: "100%" }}
-          onClose={handleSnackbarClose}
-        >
-          {snackbar.message}
-        </Alert>
-      </Snackbar>
-    </Box>
-  );
-};
-
-export const example = Template.bind({});
-
-example.args = {
+export const genericCheckIn = Template.bind({});
+genericCheckIn.args = {
   eventName: "Youth Seminar",
   eventLocation: "Kanha Shanti Vanam",
   favourites: [
@@ -141,21 +34,3 @@ example.args = {
     if (random(1)(2) === 1) throw new Error("Oops! something went wrong");
   },
 } as GenericCheckInProps;
-
-const AsyncButtonTemplate: ComponentStory<typeof AsyncButton> = (args) => (
-  <AsyncButton {...args} />
-);
-
-export const asyncButton = AsyncButtonTemplate.bind({});
-asyncButton.args = {
-  label: "Check In",
-  onClick: () =>
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (random(1)(2) === 1) resolve("success");
-        else reject();
-      }, 600);
-    }),
-  successMessage: "Prakash is checked in",
-  errorMessage: "Prakash Mishra not checked in",
-} as AsyncButtonProps;
