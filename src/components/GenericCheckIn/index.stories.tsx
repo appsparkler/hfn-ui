1adcfdde8e341817d4948a15abe0af78e84f51fb
Aakash Shah <subs@appsparkler.com>
2022-03-17 10:53:01 +0530

    chore: wip async button

--- src/components/GenericCheckIn/index.stories.tsx
+++ src/components/GenericCheckIn/index.stories.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { GenericCheckIn, GenericCheckInProps } from "./index";
 import { random, uniqueId } from "lodash/fp";
 import { action } from "@storybook/addon-actions";
+import Button from "@mui/material/Button";
+import { ClickHandler } from "../SignedInUserCheckIn";
+import { Box, CircularProgress } from "@mui/material";
 
 export default {
   title: "Components/Generic Check In",
@@ -12,6 +15,59 @@ export default {
 const Template: ComponentStory<typeof GenericCheckIn> = (args) => (
   <GenericCheckIn {...args} />
 );
+
+const label = "Sign In";
+
+export const SignInSignOutButton = ({
+  onClick = async () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 500);
+    });
+  },
+  disabled,
+}: {
+  onClick: ClickHandler;
+  disabled?: boolean;
+}) => {
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const handleClick = useCallback<ClickHandler>(
+    async (evt) => {
+      setIsProcessing(true);
+      try {
+        await onClick(evt);
+      } catch (e) {
+        throw new Error(e);
+      } finally {
+        setIsProcessing(false);
+      }
+    },
+    [onClick]
+  );
+  return (
+    <Box sx={{ position: "relative", display: "inline-block" }}>
+      <Button
+        variant="contained"
+        type="button"
+        onClick={handleClick}
+        disabled={disabled || Boolean(isProcessing)}
+      >
+        {label}
+      </Button>
+      {isProcessing && (
+        <CircularProgress
+          sx={{
+            position: "absolute",
+            left: "calc(50% - 10px)",
+            top: "calc(50% - 10px)",
+          }}
+          size={20}
+        />
+      )}
+    </Box>
+  );
+};
 
 export const example = Template.bind({});
 example.args = {
