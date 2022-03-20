039001401244a588dc78473d1b0409789118fa76
Aakash Shah <subs@appsparkler.com>
2022-03-20 07:52:50 +0530

    refactor: story renamed, etc.

--- src/components/CityStateCountryLocation/index.stories.tsx
+++ src/components/CityStateCountryLocation/index.stories.tsx
@@ -4,19 +4,17 @@ import { LocationInputField, LocationInputFieldProps } from "./index";
 import Box from "@mui/material/Box";
 
 export default {
-  title: "Components/Async SelectField",
+  title: "Components/Location Input Field",
   component: LocationInputField,
 } as ComponentMeta<typeof LocationInputField>;
 
-const AsyncButtonTemplate: ComponentStory<typeof LocationInputField> = (
-  args
-) => (
+const Template: ComponentStory<typeof LocationInputField> = (args) => (
   <Box sx={{ display: "flex", justifyContent: "center", marginY: 5 }}>
     <LocationInputField {...args} />
   </Box>
 );
-export const asyncButton = AsyncButtonTemplate.bind({});
-asyncButton.args = {
+export const locationInputField = Template.bind({});
+locationInputField.args = {
   error: false,
   label: "City, State, Country",
   required: false,
