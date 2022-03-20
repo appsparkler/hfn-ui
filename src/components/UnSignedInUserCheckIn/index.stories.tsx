c3df094b520c4544e18ce60f94cc2ae31e58648d
Aakash Shah <subs@appsparkler.com>
2022-03-20 19:34:43 +0530

    chore: setup un-signed in

---
+++ src/components/UnSignedInUserCheckIn/index.stories.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { SignedInUserCheckIn, SignedInUserCheckInProps } from "./index";
+import { uniqueId } from "lodash/fp";
+import { action } from "@storybook/addon-actions";
+import { genericCheckInVerbose } from "../GenericCheckInVerbose/index.stories";
+export default {
+  title: "Components/Un Signed In User Check In",
+  component: SignedInUserCheckIn,
+} as ComponentMeta<typeof SignedInUserCheckIn>;
+
+const Template: ComponentStory<typeof SignedInUserCheckIn> = (args) => (
+  <SignedInUserCheckIn {...args} />
+);
+
+const asyncSuccess =
+  (actionName: string, successMessage: string, timeout: number = 600) =>
+  (...args) => {
+    action(actionName)(...args);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(successMessage);
+      }, timeout);
+    });
+  };
+
+const asyncFailure =
+  (actionName: string, failureMessage: string, timeout: number = 600) =>
+  (...args) => {
+    action(actionName)(...args);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(failureMessage);
+      }, timeout);
+    });
+  };
+
+export const unSignedInUserCheckIn = Template.bind({});
+unSignedInUserCheckIn.args = {
+  onCheckInUser: asyncFailure("onCheckInUser", "user is not registered", 600),
+  onChangeVerboseUserInfo: action("onChangeVerboseUserInfo"),
+  onCheckInVerboseUser: asyncSuccess(
+    "onCheckInVerboseUser",
+    "New user successfullly checked in"
+  ),
+  unRegisteredUserInfo: genericCheckInVerbose.args.value,
+  favourites: [
+    {
+      name: "Prakash Mishra",
+      id: uniqueId("favourite-"),
+      abhyasiId: "INABC2323",
+    },
+    {
+      name: "Gayathri Devaswami",
+      id: uniqueId("favourite-"),
+      email: "ookla@dribble.com",
+    },
+  ],
+  onCheckInFavourite: asyncSuccess(
+    "oCheckInfavourite",
+    "fav is checked in",
+    600
+  ),
+  onClickCheckIn: asyncSuccess(
+    "onClickCheckIn",
+    "signed-in user is checked in",
+    600
+  ),
+  onDeleteFavourite: asyncSuccess(
+    "onDeleteFavourite",
+    "fav user is deleted",
+    600
+  ),
+  eventLocation: "Kanha Shanti Vanam",
+  eventName: "Youth Seminar",
+} as SignedInUserCheckInProps;
+unSignedInUserCheckIn.storyName = "Signed In User Check In";
---
+++ src/components/UnSignedInUserCheckIn/index.tsx
@@ -0,0 +1,156 @@
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import { FC, MouseEventHandler, useCallback, useMemo, useState } from "react";
+import MUIAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import {
+  EventNameAndLocation,
+  EventNameAndLocationProps,
+} from "../EventNameAndLocation";
+import { GenericCheckIn, GenericCheckInProps } from "../GenericCheckIn";
+import { AsyncButton } from "../AsyncButton";
+
+export type AppBarProps = {
+  onClickBackButton: ClickHandler;
+};
+
+export const AppBar: FC<AppBarProps> = ({ onClickBackButton }) => (
+  <MUIAppBar position="static">
+    <Toolbar>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={onClickBackButton}
+      >
+        <ArrowBackIcon />
+      </IconButton>
+    </Toolbar>
+  </MUIAppBar>
+);
+
+export type SignedInUserCheckInMainScreenProps = {
+  onClickCheckIn: ClickHandler;
+  onClickHelpOthersCheckIn: ClickHandler;
+};
+
+export const SignedUserCheckInMainScreen = ({
+  onClickCheckIn,
+  onClickHelpOthersCheckIn,
+}: SignedInUserCheckInMainScreenProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+      }}
+    >
+      <AsyncButton
+        variant="contained"
+        type="button"
+        size="large"
+        onClick={onClickCheckIn}
+        label="Check In"
+      />
+      <Button
+        variant="outlined"
+        type="button"
+        size="large"
+        onClick={onClickHelpOthersCheckIn}
+      >
+        Help Others Check In
+      </Button>
+    </Box>
+  );
+};
+
+export type ClickHandler = MouseEventHandler<HTMLButtonElement>;
+
+export type SignedInUserCheckInProps = {
+  onClickCheckIn: ClickHandler;
+} & GenericCheckInProps &
+  EventNameAndLocationProps;
+
+export enum SignedInUserScreen {
+  MAIN_PAGE,
+  GENERIC_CHECKIN_PAGE,
+}
+
+export const SignedInUserCheckIn: FC<SignedInUserCheckInProps> = ({
+  favourites,
+  eventLocation,
+  eventName,
+  onClickCheckIn,
+  onCheckInFavourite,
+  onDeleteFavourite,
+  onCheckInUser,
+  onChangeVerboseUserInfo,
+  onCheckInVerboseUser,
+  unRegisteredUserInfo,
+}) => {
+  const [currentPage, setCurrentPage] = useState<SignedInUserScreen>(
+    SignedInUserScreen.MAIN_PAGE
+  );
+
+  const { showMainPage, showGenericCheckInPage } = useMemo<{
+    showMainPage: boolean;
+    showGenericCheckInPage: boolean;
+  }>(() => {
+    const currentPageString = currentPage.toString();
+    return {
+      showMainPage:
+        currentPageString === SignedInUserScreen.MAIN_PAGE.toString(),
+      showGenericCheckInPage:
+        currentPageString ===
+        SignedInUserScreen.GENERIC_CHECKIN_PAGE.toString(),
+    };
+  }, [currentPage]);
+
+  const handleClickHelpOthersCheckIn = useCallback(() => {
+    setCurrentPage(SignedInUserScreen.GENERIC_CHECKIN_PAGE);
+  }, []);
+
+  const handleClickBackButton = useCallback<ClickHandler>(() => {
+    if (showMainPage) {
+    } else if (showGenericCheckInPage) {
+      setCurrentPage(SignedInUserScreen.MAIN_PAGE);
+    }
+  }, [showGenericCheckInPage, showMainPage]);
+
+  return (
+    <>
+      <AppBar onClickBackButton={handleClickBackButton} />
+      <Box sx={{ display: "flex", gap: 5 }} flexDirection="column">
+        <Box>
+          <EventNameAndLocation
+            eventName={eventName}
+            eventLocation={eventLocation}
+          />
+        </Box>
+        {showMainPage && (
+          <SignedUserCheckInMainScreen
+            onClickCheckIn={onClickCheckIn}
+            onClickHelpOthersCheckIn={handleClickHelpOthersCheckIn}
+          />
+        )}
+        {showGenericCheckInPage && (
+          <GenericCheckIn
+            favourites={favourites}
+            onCheckInFavourite={onCheckInFavourite}
+            onCheckInUser={onCheckInUser}
+            onDeleteFavourite={onDeleteFavourite}
+            onChangeVerboseUserInfo={onChangeVerboseUserInfo}
+            onCheckInVerboseUser={onCheckInVerboseUser}
+            unRegisteredUserInfo={unRegisteredUserInfo}
+          />
+        )}
+      </Box>
+    </>
+  );
+};
