6ad89bcd4f6ef5c7a65adcca140bc5d9d932556a
Aakash Shah <subs@appsparkler.com>
2022-04-06 20:16:16 +0530

    chore: removing src to init dev for bhandara-checkin

--- src/app-index.tsx
+++
@@ -1,4 +0,0 @@
-import ReactDOM from "react-dom";
-import { CheckInDemo } from "./components/CheckIn/demo";
-
-ReactDOM.render(<CheckInDemo />, document.getElementById("root"));
--- src/types/index.ts
+++
@@ -1,3 +0,0 @@
-import { MouseEventHandler } from "react";
-
-export type ClickHandler = MouseEventHandler<HTMLButtonElement>;
