dbd08abd75839a238bf30010e2c55e732bda1ef1
Aakash Shah <subs@appsparkler.com>
2022-04-09 07:47:57 +0530

    chore: add async button

---
+++ src/components/AsyncButton/AsyncButton.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { AsyncButton } from "./AsyncButton";
+
+export default {
+  title: "Components/Layouts/Async Button",
+  component: AsyncButton,
+} as ComponentMeta<typeof AsyncButton>;
+
+const Template: ComponentStory<typeof AsyncButton> = (args) => (
+  <AsyncButton {...args} />
+);
+export const asyncButton = Template.bind({});
+asyncButton.args = {
+  isProcessing: false,
+  disabled: false,
+  label: "CHECK IN",
+  size: "large",
+  variant: "contained",
+  color: "success",
+};
