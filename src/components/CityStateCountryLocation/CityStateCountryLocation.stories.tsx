cfe9b91fe8a06674cd206fc003d68e0d75c59861
Aakash Shah <subs@appsparkler.com>
2022-04-09 09:57:01 +0530

    chore: copied CityStateCountryLocation to src

---
+++ src/components/CityStateCountryLocation/CityStateCountryLocation.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import {
+  LocationInputField,
+  LocationInputFieldProps,
+} from "./CityStateCountryLocation";
+import Box from "@mui/material/Box";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Components/Location Input Field",
+  component: LocationInputField,
+} as ComponentMeta<typeof LocationInputField>;
+
+const Template: ComponentStory<typeof LocationInputField> = (args) => (
+  <Box sx={{ display: "flex", justifyContent: "center", marginY: 5 }}>
+    <LocationInputField {...args} />
+  </Box>
+);
+export const locationInputField = Template.bind({});
+locationInputField.args = {
+  error: false,
+  label: "City, State, Country",
+  required: false,
+  helperText: "Helper text for the user.",
+  size: "medium",
+  variant: "filled",
+  api: "https://static-gatsby.web.app/srcmapi/cities",
+  onChange: action("onChange"),
+} as LocationInputFieldProps;
---
+++ src/components/CityStateCountryLocation/locations.ts
@@ -0,0 +1,87 @@
+import { uniq, pipe, map, find, filter } from "lodash/fp";
+
+export type CityStateCountryLocation = {
+  id: number;
+  name: string;
+  state: string;
+  country: string;
+  country_id: number;
+  state_id: number;
+  c_id: number;
+  c_name: string;
+  active: boolean;
+};
+
+export type RefinedCityStateCountryLocation = CityStateCountryLocation & {
+  cityStateCountry: string;
+};
+
+const mapCityStateCountryLocationToRefined = map<
+  CityStateCountryLocation,
+  RefinedCityStateCountryLocation
+>((location) => ({
+  ...location,
+  cityStateCountry: (({ name, country, state }: CityStateCountryLocation) => {
+    return `${name}, ${state}, ${country}`;
+  })(location),
+}));
+
+const findWithId = <T extends { id: number }>(id: number) =>
+  find<T>((location) => id === location.id);
+
+const findWithCityStateCountry = (cityStateCountryToFind: string) =>
+  find<RefinedCityStateCountryLocation>(
+    (item) => item.cityStateCountry === cityStateCountryToFind
+  );
+
+const removeItemsThatDonotstartWithQuery = (query: string) =>
+  filter<CityStateCountryLocation>(
+    (item) =>
+      `${item.name}, ${item.state}, ${item.country}`
+        .toLowerCase()
+        .indexOf(query.toLowerCase()) > -1
+  );
+
+export const getUniqLocations = (
+  allLocations: CityStateCountryLocation[],
+  query: string
+) => {
+  const refinedLocations = pipe<
+    [CityStateCountryLocation[]],
+    CityStateCountryLocation[],
+    RefinedCityStateCountryLocation[]
+  >(
+    removeItemsThatDonotstartWithQuery(query),
+    mapCityStateCountryLocationToRefined
+  )(allLocations);
+
+  mapCityStateCountryLocationToRefined(
+    removeItemsThatDonotstartWithQuery(query)(allLocations)
+  );
+  return pipe<
+    [RefinedCityStateCountryLocation[]],
+    number[],
+    number[],
+    RefinedCityStateCountryLocation[],
+    string[],
+    string[],
+    readonly RefinedCityStateCountryLocation[]
+  >(
+    map((location) => location.id),
+    uniq,
+    map(
+      (id) =>
+        findWithId<RefinedCityStateCountryLocation>(id)(
+          refinedLocations
+        ) as RefinedCityStateCountryLocation
+    ),
+    map((location) => location.cityStateCountry),
+    uniq,
+    map(
+      (cityStateCountry) =>
+        findWithCityStateCountry(cityStateCountry)(
+          refinedLocations
+        ) as RefinedCityStateCountryLocation
+    )
+  )(refinedLocations);
+};
