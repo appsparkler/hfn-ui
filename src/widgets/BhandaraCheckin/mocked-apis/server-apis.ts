bc3175d159a028db5dafb2b947b7d38965dd186f
Aakash Shah <subs@appsparkler.com>
2022-04-16 10:10:26 +0530

    fix: url-compose error

--- src/widgets/BhandaraCheckin/mocked-apis/server-apis.ts
+++ src/widgets/BhandaraCheckin/mocked-apis/server-apis.ts
@@ -4,7 +4,7 @@ import { fetchWithToken } from "./init";
 
 export const fetchUserDetails = (abhyasiId: string): Promise<UserSRCM> =>
   fetchWithToken(
-    `https://profile.srcm.net/api/abhyasis/search/? ${new URLSearchParams({
+    `https://profile.srcm.net/api/abhyasis/search/?${new URLSearchParams({
       ref: abhyasiId.toUpperCase(),
     })}`
   ).then((res) => res.results[0]);
