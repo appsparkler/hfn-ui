027822b0c60feb20f499daad2c616a98d33616dd
Aakash Shah <subs@appsparkler.com>
2022-04-21 09:22:11 +0530

    chore: moved snackbar connnected inside its parent component folder

---
+++ src/widgets/BhandaraCheckin/BhandaraCheckin/SnackbarConnected.tsx
@@ -0,0 +1,30 @@
+import { connect, MapDispatchToProps, MapStateToProps } from "react-redux";
+import { RootState } from "../store";
+import { Snackbar, SnackbarProps } from "../../../components";
+import { snackbarSlice } from "../../../components/Snackbar/snackbarSlice";
+
+const mapStateToProps: MapStateToProps<SnackbarProps, {}, RootState> = ({
+  snackbar: { open, vertical, horizontal, severity, children, variant },
+}) => {
+  return {
+    open,
+    vertical,
+    horizontal,
+    severity,
+    children,
+    variant,
+  };
+};
+
+const mapDispatchToProps: MapDispatchToProps<SnackbarProps, {}> = (
+  dispatch
+) => {
+  return {
+    onClose: () => dispatch(snackbarSlice.actions.closeSnackbar()),
+  };
+};
+
+export const SnackbarConnected = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Snackbar);
