4ba2f0cb40332d614bfe4b3c0397fe018b614c7e
Aakash Shah <subs@appsparkler.com>
2022-04-24 10:38:28 +0530

    chore: update age-group values

--- src/widgets/BhandaraCheckin/constants/index.ts
+++ src/widgets/BhandaraCheckin/constants/index.ts
@@ -16,83 +16,83 @@ export const maxWidth = 350;
 
 export const ageGroupOptions: SelectFieldOption[] = [
   {
-    value: "0-4",
+    value: "4",
     label: "0-4",
   },
   {
-    value: "5-9",
+    value: "9",
     label: "5-9",
   },
   {
-    value: "10-14",
+    value: "14",
     label: "10-14",
   },
   {
-    value: "15-19",
+    value: "19",
     label: "15-19",
   },
   {
-    value: "20-24",
+    value: "24",
     label: "20-24",
   },
   {
-    value: "25-29",
+    value: "29",
     label: "25-29",
   },
   {
-    value: "30-34",
+    value: "34",
     label: "30-34",
   },
   {
-    value: "35-39",
+    value: "39",
     label: "35-39",
   },
   {
-    value: "40-44",
+    value: "44",
     label: "40-44",
   },
   {
-    value: "45-49",
+    value: "49",
     label: "45-49",
   },
   {
-    value: "50-54",
+    value: "54",
     label: "50-54",
   },
   {
-    value: "55-59",
+    value: "59",
     label: "55-59",
   },
   {
-    value: "60-64",
+    value: "64",
     label: "60-64",
   },
   {
-    value: "65-69",
+    value: "69",
     label: "65-69",
   },
   {
-    value: "70-74",
+    value: "74",
     label: "70-74",
   },
   {
-    value: "75-79",
+    value: "79",
     label: "75-79",
   },
   {
-    value: "80-84",
+    value: "84",
     label: "80-84",
   },
   {
-    value: "85-89",
+    value: "89",
     label: "85-89",
   },
   {
-    value: "90-94",
+    value: "94",
     label: "90-94",
   },
   {
-    value: "95-99",
+    value: "99",
     label: "95-99",
   },
 ];
