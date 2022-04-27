81c2850923a9cd492d1f3810efed8aa35580a633
Aakash Shah <subs@appsparkler.com>
2022-04-27 07:19:22 +0530

    chore: api-async-thunk

--- src/widgets/BhandaraCheckin/store/api-async-thunks/maskedPnrs.ts
+++ src/widgets/BhandaraCheckin/store/api-async-thunks/maskedPnrs.ts
@@ -1,14 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { isEmail, isMobile } from "utils";
+import { isEmail, isMobile, isPnr } from "utils";
 import { ThunkApiConfig } from "..";
 
 export enum MaskedPnrRejectReason {
   NO_RESULTS,
+  INVALID_QUERY,
   SERVER_ERROR,
 }
 
 export const maskedPnrs = createAsyncThunk<
-  { id: string; name: string; isCheckin: boolean }[],
+  { id: string; name: string; checkedIn: boolean }[],
   string,
   ThunkApiConfig
 >(
@@ -22,19 +23,22 @@ export const maskedPnrs = createAsyncThunk<
       rejectWithValue,
     }
   ) => {
+    const getValue = ($value: string) => {
+      if (isEmail($value)) return { email: $value };
+      if (isMobile($value)) return { mobile: $value };
+      if (isPnr($value)) return { pnr: $value };
+    };
     try {
-      const isMobileOrPnr = isMobile(registeringWithValue)
-        ? { mobile: registeringWithValue }
-        : { pnr: registeringWithValue };
-      const value = isEmail(registeringWithValue)
-        ? { email: registeringWithValue }
-        : isMobileOrPnr;
-      const res = await maskedPnrs(value);
-      if (res.results.length > 0) {
-        return [];
-      } else {
-        return rejectWithValue(MaskedPnrRejectReason.NO_RESULTS);
+      const queryParams = getValue(registeringWithValue);
+      if (queryParams) {
+        const res = await maskedPnrs(queryParams);
+        if (res.results.length > 0) {
+          return res.results;
+        } else {
+          return rejectWithValue(MaskedPnrRejectReason.NO_RESULTS);
+        }
       }
+      return rejectWithValue(MaskedPnrRejectReason.INVALID_QUERY);
     } catch (error) {
       return rejectWithValue(MaskedPnrRejectReason.SERVER_ERROR);
     }
