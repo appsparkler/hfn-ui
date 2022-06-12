61c7ad76066de13a1983b049d2df31b73cefc062
Aakash Shah <subs@appsparkler.com>
2022-06-12 09:50:31 +0530

    chore: fix lint issue

--- src/components/BarcodeScanner/barcodeScannerSlice.tsx
+++ src/components/BarcodeScanner/barcodeScannerSlice.tsx
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { WritableDraft } from "immer/dist/internal";
 import { BarcodeScannerStateProps } from "./BarcodeScanner";
 
 export const barcodeScannerSlice = createSlice({
