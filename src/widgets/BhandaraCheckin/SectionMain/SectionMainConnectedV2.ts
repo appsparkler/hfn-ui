d78dfd65af65e879cd70fe82abc55ee1dbecfbdb
Aakash Shah <subs@appsparkler.com>
2022-07-03 06:42:18 +0530

    chore: working V2

--- src/widgets/BhandaraCheckin/SectionMain/SectionMainConnectedV2.ts
+++ src/widgets/BhandaraCheckin/SectionMain/SectionMainConnectedV2.ts
@@ -1,14 +1,16 @@
-import { connect, MapStateToProps } from "react-redux";
+import { connect, MapDispatchToProps, MapStateToProps } from "react-redux";
+import { RootState } from "widgets/BhandaraCheckin/store";
 import {
-  RootState,
-  mainSectionMapDispatchToProps,
-} from "widgets/BhandaraCheckin/store";
-import { SectionMain, SectionMainStateProps } from "./SectionMain";
+  SectionMain,
+  SectionMainDispatchProps,
+  SectionMainStateProps,
+} from "./SectionMain";
+import { mapDispatchToProps } from "./mapDispatchToProps";
 
 const mapStateToProps: MapStateToProps<SectionMainStateProps, {}, RootState> =
   ({ mainSection }) => mainSection;
 
 export const SectionMainConnectedV2 = connect(
-  mapStateToProps
-  // mainSectionMapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(SectionMain);
