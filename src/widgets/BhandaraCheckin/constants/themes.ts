35c7322bc97134f452355f22a6e13595ba8b3079
Aakash Shah <subs@appsparkler.com>
2022-07-08 22:29:20 +0530

    chore: theme setup

--- src/widgets/BhandaraCheckin/constants/themes.ts
+++ src/widgets/BhandaraCheckin/constants/themes.ts
@@ -1,26 +1,19 @@
 import { createTheme } from "@mui/material";
 import { ModeProviderStateProps } from "components";
 
-const lightTheme = createTheme({ palette: { mode: "dark" } });
-
-const darkTheme = createTheme({ palette: { mode: "dark" } });
 export const themes: ModeProviderStateProps["themes"] = {
-  dark: {
-    ...darkTheme,
-    palette: {
-      ...darkTheme.palette,
-      error: {
-        ...darkTheme.palette.success,
-      },
-    },
-  },
-  light: {
-    ...lightTheme,
-    palette: {
-      ...lightTheme.palette,
-      error: {
-        ...lightTheme.palette.success,
-      },
-    },
-  },
+  dark: getTheme("dark"),
+  light: getTheme("light"),
 };
+function getTheme(mode: ModeProviderStateProps["mode"]) {
+  const theme = createTheme({ palette: { mode } });
+  return {
+    ...theme,
+    palette: {
+      ...theme.palette,
+      error: {
+        ...theme.palette.success,
+      },
+    },
+  };
+}
