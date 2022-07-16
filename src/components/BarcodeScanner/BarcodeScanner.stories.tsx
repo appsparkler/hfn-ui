eebd7f2200418c69de26378eeba13a405962f406
Aakash Shah <subs@appsparkler.com>
2022-07-16 18:28:18 +0530

    chore: setup bar-code-scanner v2

--- src/components/BarcodeScanner/BarcodeScanner.stories.tsx
+++ src/components/BarcodeScanner/BarcodeScanner.stories.tsx
@@ -1,14 +1,27 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { BarcodeScanner } from "./BarcodeScanner";
+import { useCallback } from "react";
+import { BarcodeScanner, BarcodeScannerProps } from "./BarcodeScanner";
 
 export default {
   title: "Components/Scanners/Barcode Scanner",
   component: BarcodeScanner,
 } as ComponentMeta<typeof BarcodeScanner>;
 
-const Template: ComponentStory<typeof BarcodeScanner> = (args) => (
-  <BarcodeScanner {...args} />
-);
+const Template: ComponentStory<typeof BarcodeScanner> = (args) => {
+  const handleMount = useCallback<BarcodeScannerProps["onMount"]>(
+    (videoRef) => {
+      args.onMount(videoRef);
+      if (videoRef.current) {
+        videoRef.current.autoplay = true;
+        videoRef.current.controls = true;
+        videoRef.current.innerHTML = `<source src = "https://www.w3schools.com/html/mov_bbb.mp4">`;
+      }
+    },
+    [args]
+  );
+
+  return <BarcodeScanner {...args} onMount={handleMount} />;
+};
 export const barcodeScanner = Template.bind({});
 barcodeScanner.args = {
   show: true,
