4003c0ed70113514310da0a131e0897684d54d98
aakashshah1 <aakash.shah1@tesco.com>
2022-08-29 15:51:53 +0530

    chore: remove dashboard-v0

--- src/widgets/BhandaraCheckin/types/components/Dashboard.ts
+++ src/widgets/BhandaraCheckin/types/components/Dashboard.ts
@@ -1,18 +1,5 @@
 import { CheckinsAggregateData } from "../aggregations";
 
-export interface DashboardV0StateProps {
-  password?: string;
-  total?: number;
-}
-
-export interface DashboardV0DispatchProps {
-  onRefresh?: () => void;
-  onMount?: () => void;
-  onReturn?: () => void;
-}
-
-export type DashboardV0Props = DashboardV0StateProps & DashboardV0DispatchProps;
-
 export interface DashboardStateProps {
   stats: CheckinsAggregateData;
 }
