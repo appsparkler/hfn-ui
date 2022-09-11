ad34182482c8ba2805074b10f3057725b86aee28
Aakash Shah <subs@appsparkler.com>
2022-09-11 18:53:56 +0530

    chore: remove env from collection path

--- src/widgets/BhandaraCheckin/types/firebase.ts
+++ src/widgets/BhandaraCheckin/types/firebase.ts
@@ -1,7 +1,5 @@
-import { getEnv } from "../utils";
-
 export const FirestoreCollections = {
   ABHYASI_ID_CHECKINS: "abhyasiId-checkins",
   OTHER_CHECKINS: "other-checkins",
-  CHECKINS: `checkins-${getEnv().NODE_ENV}`,
+  CHECKINS: `checkins`,
 };
