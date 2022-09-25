8c96c2cbea7267823d8c414c66841b32364828df
Aakash Shah <subs@appsparkler.com>
2022-09-25 11:52:07 +0530

    chore: include AppUpdater in root-view

--- src/widgets/BhandaraCheckin/components/AppUpdater/AppUpdater.tsx
+++ src/widgets/BhandaraCheckin/components/AppUpdater/AppUpdater.tsx
@@ -3,7 +3,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { CenterOfViewport, Vertical } from "components";
 import { Typography } from "@mui/material";
 
-export function AppUpdater({ progressText }: { progressText: string }) {
+export function AppUpdater({ progressText }: { progressText?: string }) {
   return (
     <CenterOfViewport>
       <Vertical alignItems={"center"} gap={2}>
--- src/widgets/BhandaraCheckin/components/BhandaraCheckin/BhandaraCheckinView.tsx
+++ src/widgets/BhandaraCheckin/components/BhandaraCheckin/BhandaraCheckinView.tsx
@@ -4,30 +4,39 @@ import { pages } from "widgets/BhandaraCheckin/routing";
 import { Box } from "@mui/material";
 import { BarcodeScannerConnected } from "./BarcodeScannerConnected";
 import { AppStatusIndicator } from "components";
+import { AppUpdater } from "../AppUpdater";
 
 export type BhandaraCheckinWidgetProps = {
   apis: BhandaraCheckinAPIs;
 };
 
 export type BhandaraCheckinViewStateProps = {
+  renderApp?: boolean;
   renderScanner?: boolean;
   page?: PageEnum;
 };
 
 export const BhandaraCheckinView = ({
   page = PageEnum.Home,
+  renderApp = false,
   renderScanner,
 }: BhandaraCheckinViewStateProps) => {
   const Component = (pages as any)[page] as any;
 
   return (
     <Box>
-      <Component />
-      <SnackbarConnected />
-      {renderScanner ? <BarcodeScannerConnected /> : null}
+      {renderApp ? (
+        <>
+          <Component />
+          {renderScanner ? <BarcodeScannerConnected /> : null}
+        </>
+      ) : (
+        <AppUpdater />
+      )}
       <Box position="fixed" left={2} bottom={2}>
         <AppStatusIndicator />
       </Box>
+      <SnackbarConnected />
     </Box>
   );
 };
