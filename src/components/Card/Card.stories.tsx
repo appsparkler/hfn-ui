a31722e7ac252f9103185c7e08c86d3fe66d0919
Aakash Shah <subs@appsparkler.com>
2022-11-27 17:27:13 +0530

    chore: wip

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -6,13 +6,16 @@ const Story = {
   title: "Components/Card",
 } as ComponentMeta<typeof MultiActionAreaCard>;
 
-const Template: ComponentStory<typeof MultiActionAreaCard> = (args = {}) => (
-  <MultiActionAreaCard />
+const Template: ComponentStory<typeof MultiActionAreaCard> = (args) => (
+  <MultiActionAreaCard {...args} />
 );
 
 export const card: ComponentStory<typeof MultiActionAreaCard> = Template.bind(
   {}
 );
-card.args = {};
+card.args = {
+  fullName: "Shekhar Kapoor",
+  id: "tile-1",
+};
 
 export default Story;
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,54 +8,47 @@ import {
   CardActions,
   Checkbox,
   FormControlLabel,
+  Input,
+  TextField,
 } from "@mui/material";
 import { Horizontal, Vertical } from "components/Boxes";
+import { Box } from "@mui/system";
 
 export function Tiles() {
-  return [{ id: "tile-1" }, { id: "tile-2" }].map((tileData) => (
-    <MultiActionAreaCard key={tileData.id} {...tileData} />
-  ));
+  return [
+    { id: "tile-1", fullName: "Jane Mathew" },
+    { id: "tile-2", fullName: "Shekhar Kapoor" },
+  ].map((tileData) => <MultiActionAreaCard key={tileData.id} {...tileData} />);
 }
 
-export function MultiActionAreaCard() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardContent>
-        <Vertical>
-          <FormControlLabel
-            control={
-              <Checkbox checked={true} onChange={() => {}} name="jason" />
-            }
-            label="Jane Mathew"
-          />
-          <Typography variant="h6">Dorm Preference</Typography>
-          <Typography variant="body2" color="text.secondary">
-            Comfort Dorm B1, LB
-          </Typography>
-        </Vertical>
-      </CardContent>
-      <CardActionArea>
-        {/* <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        /> */}
+export const MultiActionAreaCard: React.FC<{ fullName: string; id: string }> =
+  ({ fullName }) => {
+    return (
+      <Card sx={{ maxWidth: 345 }}>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
+          <Vertical gap={1}>
+            <FormControlLabel
+              control={
+                <Checkbox checked={true} onChange={() => {}} name="jason" />
+              }
+              label={fullName}
+            />
+            <Box>
+              <Typography variant="subtitle2">Dorm Preference</Typography>
+              <Typography variant="body2" color="text.secondary">
+                Comfort Dorm B1, LB
+              </Typography>
+            </Box>
+            <Box width="100%">
+              <Typography variant="subtitle2">Dorm Allocation</Typography>
+              <TextField
+                variant="outlined"
+                placeholder="Please enter allocated dorm and birth..."
+                fullWidth
+              />
+            </Box>
+          </Vertical>
         </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-  );
-}
+      </Card>
+    );
+  };
