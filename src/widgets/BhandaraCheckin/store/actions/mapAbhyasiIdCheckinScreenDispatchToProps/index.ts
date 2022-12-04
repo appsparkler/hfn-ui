6763fa6c98879f4c81c7c1b61439a39e3436d857
Aakash Shah <subs@appsparkler.com>
2022-12-04 14:51:33 +0530

    chore: set dorm/birth allocation details

--- src/widgets/BhandaraCheckin/store/actions/mapAbhyasiIdCheckinScreenDispatchToProps/index.ts
+++ src/widgets/BhandaraCheckin/store/actions/mapAbhyasiIdCheckinScreenDispatchToProps/index.ts
@@ -1,13 +1,17 @@
 import { MapDispatchToProps } from "react-redux";
+import { Action, Dispatch } from "redux";
 import { IAbhyasiIDCheckinScreenDispatchProps } from "widgets/BhandaraCheckin/types";
+import { abhyasiIdCheckinScreenActions } from "../../slices/abhyasiIdCheckinScreen";
 
 export const mapAbhyasiIDCheckinScreenDispatchToProps: MapDispatchToProps<
   IAbhyasiIDCheckinScreenDispatchProps,
   {}
-> = () => {
+> = (dispatch: Dispatch<Action<any>>) => {
   return {
     onCancel: console.log,
     onCheckin: console.log,
-    onChangeDormAndBirthAllocation: console.log,
+    onChangeDormAndBirthAllocation: ({ target: { value } }) => {
+      dispatch(abhyasiIdCheckinScreenActions.setDormAndBirthAllocation(value));
+    },
   };
 };
