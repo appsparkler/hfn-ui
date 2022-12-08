7bf36cebd260ad30b7b7a8bc40c2f5caabe39816
Aakash Shah <subs@appsparkler.com>
2022-12-09 05:16:40 +0530

    chore: wip

---
+++ src/widgets/BhandaraCheckin/store/api-async-thunks/checkinWithQRCode.ts
@@ -0,0 +1,21 @@
+import { IQRCheckinUser } from "@hfn-checkins/types";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { ThunkApiConfig } from "widgets/BhandaraCheckin/types";
+
+export const multiCheckinWithQRCode = createAsyncThunk<
+  any,
+  IQRCheckinUser[],
+  ThunkApiConfig
+>(
+  "api/multiCheckinWithQRCode",
+  (
+    qrCodeUsers,
+    {
+      extra: {
+        apis: { checkinWithQRCode },
+      },
+    }
+  ) => {
+    checkinWithQRCode(qrCodeUsers);
+  }
+);
