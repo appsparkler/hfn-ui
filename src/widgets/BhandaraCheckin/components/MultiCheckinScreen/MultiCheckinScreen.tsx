ce3397a47c9546bae4bdcef53507cdcd120b2157
Aakash Shah <subs@appsparkler.com>
2022-12-09 05:39:12 +0530

    chore: wip

--- src/widgets/BhandaraCheckin/components/MultiCheckinScreen/MultiCheckinScreen.tsx
+++ src/widgets/BhandaraCheckin/components/MultiCheckinScreen/MultiCheckinScreen.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Card, CardContent, Typography } from "@mui/material";
 import { Horizontal, Vertical } from "components";
 import { CheckinInfoTiles } from "./CheckinInfoTile";
 import { TMultiCheckinScreenComponent } from "widgets/BhandaraCheckin/types";
+import { maxWidth } from "widgets/BhandaraCheckin/constants";
 
 export const MultiCheckinScreen: TMultiCheckinScreenComponent = ({
   userData,
@@ -11,10 +12,23 @@ export const MultiCheckinScreen: TMultiCheckinScreenComponent = ({
   onChangeData,
 }) => {
   return (
-    <Vertical sx={{ display: "flex", height: "100vh" }}>
-      <Box boxShadow="2px 2px 5px black" zIndex={2000}>
+    <Vertical
+      sx={{
+        display: "flex",
+        maxWidth,
+        marginX: "auto",
+      }}
+    >
+      <Box
+        boxShadow="0px 2px 5px black"
+        zIndex={2000}
+        position="sticky"
+        top={0}
+        p={1}
+        sx={{ backgroundColor: "background.paper" }}
+      >
         <Typography align="center" variant="h6" py="2">
-          Checkin with Dorm/Bed Allocation
+          QR Code Checkin
         </Typography>
       </Box>
       <Box sx={{ flex: 1, overflow: "auto", py: 2 }}>
@@ -36,7 +50,6 @@ export const MultiCheckinScreen: TMultiCheckinScreenComponent = ({
         sx={{
           justifyContent: "space-around",
           padding: 2,
-          boxShadow: "-2px -2px 5px black ",
         }}
       >
         <Button onClick={onClickCancel} size="large">
