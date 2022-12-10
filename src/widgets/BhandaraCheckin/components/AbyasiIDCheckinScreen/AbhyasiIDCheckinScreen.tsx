a90cfe3c3b9c07dd7059c7e42beadfe13d62e6e3
Aakash Shah <subs@appsparkler.com>
2022-12-10 12:01:10 +0530

    chore: update layout of abhyasi-id checkin screen

--- src/widgets/BhandaraCheckin/components/AbyasiIDCheckinScreen/AbhyasiIDCheckinScreen.tsx
+++ src/widgets/BhandaraCheckin/components/AbyasiIDCheckinScreen/AbhyasiIDCheckinScreen.tsx
@@ -6,8 +6,9 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { CenterOfViewport, Horizontal, Vertical } from "components";
+import { Horizontal, Vertical } from "components";
 import { useEffect, useRef } from "react";
+import { maxWidth } from "widgets/BhandaraCheckin/constants";
 import { TAbhyasiIDCheckinScreenComponentProps } from "widgets/BhandaraCheckin/types/screens";
 
 export const AbhyasiIDCheckinScreen: TAbhyasiIDCheckinScreenComponentProps = ({
@@ -21,8 +22,20 @@ export const AbhyasiIDCheckinScreen: TAbhyasiIDCheckinScreenComponentProps = ({
     inputRef.current?.focus();
   }, []);
   return (
-    <CenterOfViewport>
-      <Box width="100%" p={2}>
+    <Vertical maxWidth={maxWidth} marginX="auto">
+      <Vertical gap={2}>
+        <Box
+          boxShadow="0px 2px 5px black"
+          zIndex={2000}
+          position="sticky"
+          top={0}
+          p={1}
+          sx={{ backgroundColor: "background.paper" }}
+        >
+          <Typography align="center" variant="h6" py="2">
+            Abhyasi ID Checkin
+          </Typography>
+        </Box>
         <Card elevation={1}>
           <CardContent>
             <Vertical gap={1}>
@@ -69,7 +82,7 @@ export const AbhyasiIDCheckinScreen: TAbhyasiIDCheckinScreenComponentProps = ({
             </Horizontal>
           </CardContent>
         </Card>
-      </Box>
-    </CenterOfViewport>
+      </Vertical>
+    </Vertical>
   );
 };
