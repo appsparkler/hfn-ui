bec29a816af4e7813b1a9e00c280271ab7e9cf6a
Aakash Shah <subs@appsparkler.com>
2023-01-31 06:05:40 +0530

    chore: wip bulk checkin

--- src/widgets/BhandaraCheckin/components/FileParserApp/FileParserApp.stories.tsx
+++ src/widgets/BhandaraCheckin/components/FileParserApp/FileParserApp.stories.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable */
-import { CheckinTypesEnum } from "@hfn-checkins/types";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { doc, setDoc, writeBatch } from "firebase/firestore";
+import { getAuth, signInAnonymously, signOut } from "firebase/auth";
+import { doc, runTransaction } from "firebase/firestore";
+import { useEffect } from "react";
 import { firestoreDb } from "widgets/BhandaraCheckin/firebase";
 import { FirestoreCollections } from "widgets/BhandaraCheckin/types";
 import { FileParserApp } from "./FileParserApp";
@@ -12,12 +12,35 @@ const Story = {
 } as ComponentMeta<typeof FileParserApp>;
 
 const Template: ComponentStory<typeof FileParserApp> = (args) => {
+  useEffect(() => {
+    signInAnonymously(getAuth());
+    return () => {
+      signOut(getAuth());
+    };
+  }, []);
+
   const SELECTED_SHEET = "Sorted";
-  const batch = writeBatch(firestoreDb);
   const handleData = async (sheetName: string, data: Record<any, any>[]) => {
     if (sheetName === SELECTED_SHEET) {
       const first10Items = data.slice(0, 10);
       console.log(first10Items);
+      runTransaction(firestoreDb, async (transaction) => {
+        first10Items.forEach((eachItem) => {
+          const uniqueId = eachItem.uniqueId;
+          const docRef = doc(
+            firestoreDb,
+            FirestoreCollections.CHECKINS,
+            uniqueId
+          );
+          transaction.set(docRef, {
+            ...eachItem,
+            timestamp: Date.now(),
+            type: "FileUpload",
+            updatedInReport: false,
+            deviceId: "admin",
+          });
+        });
+      });
       // first10Items.forEach((eachItem) => {
       //   const uniqueId = eachItem.uniqueId;
       //   const docRef = doc(
