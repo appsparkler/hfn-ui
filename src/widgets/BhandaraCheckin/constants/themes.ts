c892854a9991404d0bc9bb2b8f18f95cb9c292a6
Aakash Shah <appsparkler@gmail.com>
2023-09-06 12:19:05 +0530

    chore: pass dark/light background

--- src/widgets/BhandaraCheckin/constants/themes.ts
+++ src/widgets/BhandaraCheckin/constants/themes.ts
@@ -1,12 +1,30 @@
 import { createTheme } from "@mui/material";
 import { ModeProviderStateProps } from "components";
+import { textStrings } from "./textStrings";
 
 export const themes: ModeProviderStateProps["themes"] = {
   dark: getTheme("dark"),
   light: getTheme("light"),
 };
 function getTheme(mode: ModeProviderStateProps["mode"]) {
-  const theme = createTheme({ palette: { mode } });
+  const theme = createTheme({
+    palette: { mode },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            backgroundSize: "100% 100%",
+            backgroundAttachment: "fixed",
+            backgroundRepeat: "no-repeat",
+            background:
+              "dark" === mode
+                ? `url(${textStrings.bgImage_dark})`
+                : `url(${textStrings.bgImage_light})`,
+          },
+        },
+      },
+    },
+  });
   return {
     ...theme,
     palette: {
