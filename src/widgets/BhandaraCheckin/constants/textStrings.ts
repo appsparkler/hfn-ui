efbe207f291ce311fe2330a52abde1679c6ec8aa
Aakash Shah <appsparkler@gmail.com>
2023-09-06 12:18:53 +0530

    chore: update celebration details

--- src/widgets/BhandaraCheckin/constants/textStrings.ts
+++ src/widgets/BhandaraCheckin/constants/textStrings.ts
@@ -2,11 +2,11 @@ export const textStrings = {
   batchId: "2023_september_bhandara",
   hfn_logo_imgUrl: "heartfulness_blue.png",
 
-  bgImage_light: "bg_light.png",
+  bgImage_light: "bg_light.jpg",
   bgImage_dark: "bg_dark.png",
 
   hfn_logo_alt: "Heartfulness - Advancing in Love",
-  eventTitle: "96th Birth Anniversary of Pujya Shri Chariji Maharaj",
+  eventTitle: "2023 Birth Anniversary Celebrations of Pujya Daaji",
   getCheckinsCollectionPath: function () {
     return `events/${this.batchId}/checkins`;
   },
