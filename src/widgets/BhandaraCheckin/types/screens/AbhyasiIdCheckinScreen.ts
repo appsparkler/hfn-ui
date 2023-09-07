e0dfebf4bee84b5752869d766465f7e9e169f475
Aakash Shah <appsparkler@gmail.com>
2023-09-07 11:12:39 +0530

    chore: passing "batch" to abhyasi-id-checkin screen

--- src/widgets/BhandaraCheckin/types/screens/AbhyasiIdCheckinScreen.ts
+++ src/widgets/BhandaraCheckin/types/screens/AbhyasiIdCheckinScreen.ts
@@ -1,7 +1,9 @@
 import { TextFieldPropsOnChange } from "types";
+import { Batch } from "../components";
 
 export interface IAbhyasiIDCheckinScreenStateProps {
   abhyasiId: string;
+  batch: Batch;
   dormAndBerthAllocation: string;
 }
 
