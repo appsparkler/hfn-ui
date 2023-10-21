808f4d36a486007e93703c2b21edcc21f2dc95e6
Aakash Shah <appsparkler@gmail.com>
2023-10-21 09:38:02 +0530

    chore: successfully generate reports

--- hfn-ui-reports/index.ts
+++ hfn-ui-reports/index.ts
@@ -1,8 +1,11 @@
 import * as admin from "firebase-admin/app";
 import * as firestore from "firebase-admin/firestore";
 import adminCreds from "./hfn-checkins-firebase-adminsdk-uod1b-375d663d8e.json";
+import * as xlsx from "xlsx";
+import * as fs from "fs";
 
-const { initializeApp, applicationDefault, cert } = admin;
+// xlsx.set_fs(fs);
+const { cert } = admin;
 
 const { getFirestore } = firestore;
 
@@ -10,17 +13,49 @@ admin.initializeApp({
   credential: cert(adminCreds as admin.ServiceAccount),
 });
 
+const createJsonFile = (fileName: string, data: any[]) => {
+  fs.writeFileSync(fileName, JSON.stringify(data), "utf-8");
+  console.log(`${data.length} docs written to JSON file ${fileName}`);
+};
+
+const createXLSXFile = (fileName: string, data: any[]) => {
+  const workSheet = xlsx.utils.json_to_sheet(data);
+  const workBook = xlsx.utils.book_new();
+  xlsx.utils.book_append_sheet(workBook, workSheet, "Sheet1");
+  xlsx.writeFile(workBook, fileName);
+};
+
 const db = getFirestore();
 
-const collection = db.collection("/events/202307_july_bhandara/checkins");
-
-const docRef = collection.doc("633d53fe-443d-4ff4-b3b0-7bd13dabf303");
-
-docRef.get().then((doc) => {
-  if (doc.exists) {
-    console.log("Document data:", doc.data());
-  } else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
+const getData = async (collectionPath: string) => {
+  const collection = db.collection(collectionPath);
+  const docRef = collection.where("type", "==", "EmailOrMobile");
+  const snapshot = await docRef.get();
+  if (snapshot.empty) {
+    console.log("no matching docs");
+    return;
   }
-});
+  const data: any[] = [];
+  snapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+  return data;
+};
+
+const init = () => {
+  createXLSXFromJSON("./data.json", "./data.xlsx");
+  // const data = await getData("/events/202307_july_bhandara/checkins");
+  // if (data) {
+  //   createJsonFile("./data.json", data);
+  //   createXLSXFile("./data.xlsx", data);
+  // }
+};
+
+const createXLSXFromJSON = (jsonFileName: string, xlsxFileName: string) => {
+  const buffer = fs.readFileSync(jsonFileName);
+  const data = JSON.parse(buffer.toString());
+  console.log(data.length);
+  createXLSXFile(xlsxFileName, data);
+};
+
+init();
