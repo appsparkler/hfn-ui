0da5c6d82316b3cacfd4aca4d4d744c344c56e01
Aakash Shah <appsparkler@gmail.com>
2024-03-09 22:24:51 +0530

    chore: add manual entry user

---
+++ src/widgets/GSM/model/ManualEntryUser.ts
@@ -0,0 +1,9 @@
+export interface ManualEntryUser {
+    name: string;
+    mobileNo: string;
+    email: string;
+    organization: string;
+    checkinTime: number;
+    uid: string | null;
+    platform: "WEB"
+}
