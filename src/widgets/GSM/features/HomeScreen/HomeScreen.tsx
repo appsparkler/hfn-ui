969e227a0abb797e3f8537cc8b04cd78448f0f02
Aakash Shah <appsparkler@gmail.com>
2024-03-09 15:50:07 +0530

    chore: wip

--- src/widgets/GSM/features/HomeScreen/HomeScreen.tsx
+++ src/widgets/GSM/features/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,99 @@
+import { Box, Card, CardContent, FormControlLabel, Switch, TextField, Typography } from "@mui/material";
+import { Horizontal, Vertical } from "components";
+import { noop } from "lodash/fp";
 import React from "react";
+
+const maxWidth = 420
 export const HomeScreen: React.FC<{}> = () => {
-  return <>Home Screen</>;
+  return (
+    <Vertical mx="auto" justifyContent={"center"} alignItems={"center"}>
+      <Card sx={{ mt: 6, maxWidth: maxWidth, opacity: 0.86 }}>
+        <CardContent>
+          <Vertical alignItems={"center"} gap={2}>
+            <img
+              src={""}
+              alt={""}
+              width="230"
+            />
+            <Typography variant="h5" align="center">
+              Global Spirituality Mahotsav
+            </Typography>
+            {/* <SelectField
+              size="medium"
+              autoWidth
+              autoComplete="off"
+              label="Batch"
+              labelId="batch"
+              name="batch"
+              onChange={}
+              options={batchOptions}
+              required
+              value={batch}
+            /> */}
+            <TextField
+              type="text"
+              label="Abhyasi ID / Mobile # / Email"
+              variant="outlined"
+              required
+              autoComplete="off"
+              // error={error}
+              // value={value}
+              // onChange={handleChange}
+              // helperText={helperText}
+              // inputRef={idFieldRef}
+              fullWidth
+              FormHelperTextProps={{
+                sx: {
+                  maxHeight: 38,
+                  height: 38,
+                },
+              }}
+            />
+            <Horizontal gap={1}>
+              {/* <AsyncButton
+                type="button"
+                onClick={handleClickStart}
+                disabled={!isStartButtonEnabled}
+                isProcessing={isProcessing}
+                size="large"
+              >
+                START CHECK IN
+              </AsyncButton> */}
+              {/* <AsyncButton
+                color="warning"
+                startIcon={<BarcodeSVG />}
+                disabled={scanBtnProcessing || scanBtnDisabled}
+                isProcessing={scanBtnProcessing}
+                onClick={onClickScan}
+                size="large"
+                endIcon={<QrCode2 fontSize="medium" />}
+              ></AsyncButton> */}
+            </Horizontal>
+
+            <Vertical>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={false}
+                    disabled={false}
+                    onChange={noop}
+                  />
+                }
+                label="Scanner"
+              />
+            </Vertical>
+            <Box position="fixed" right={0} top={0}>
+              <Horizontal alignItems={"center"}>
+                {/* <ModeSwitch checked={false} onSwitch={onSwitchMode} /> */}
+                {/* <CustomMenu
+                  onClickDashboard={onClickDashboard}
+                  onRefreshApp={onRefresh}
+                /> */}
+              </Horizontal>
+            </Box>
+          </Vertical>
+        </CardContent>
+      </Card>
+    </Vertical>
+  );
 };
