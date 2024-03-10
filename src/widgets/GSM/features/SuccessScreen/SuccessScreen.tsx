ef6bfc9db76cff47d8667988655e9ff402da0b6a
Aakash Shah <appsparkler@gmail.com>
2024-03-10 15:04:53 +0530

    chore: wip

--- src/widgets/GSM/features/SuccessScreen/SuccessScreen.tsx
+++ src/widgets/GSM/features/SuccessScreen/SuccessScreen.tsx
@@ -10,62 +10,88 @@ export const SuccessScreen: React.FC<{
   manualEntryUser: ManualEntryUser | null;
 }> = ({ manualEntryUser }) => {
   return (
-    <>
+    <Vertical
+      alignItems={"center"}
+      justifyContent={"center"}
+      sx={{
+        width: "100%",
+        maxWidth: "420px",
+        mx: "auto",
+      }}
+      gap={2}
+    >
       <img src="image.webp" width={"100%"} alt="minister" />
-      <Vertical alignItems={"center"}>
-        <CheckCircleIcon
-          color="success"
+      <CheckCircleIcon
+        color="success"
+        sx={{
+          fontSize: 80,
+        }}
+      />
+      <Typography variant="h5">Checked In</Typography>
+      <Card
+        sx={{
+          width: "100%",
+          maxWidth: "420px",
+          mx: 1
+        }}
+      >
+        <Typography
           sx={{
-            fontSize: 80,
+            p: 1,
+            bgcolor: (theme) => theme.palette.primary.main,
+            color: "primary.contrastText",
           }}
-        />
-        <Typography variant="h5">
-          Checked In
+          variant="h5"
+        >
+          Checkin Details
         </Typography>
-        <Card
-          sx={{
-            width: "100%",
-            maxWidth: "420px"
-          }}>
-          <Typography
-            sx={{
-              p: 1,
-              bgcolor: theme => theme.palette.primary.main,
-              color: "primary.contrastText",
-            }}
-            variant="h5"
-          >
-            Checkin Details
-          </Typography>
-          <CardContent>
-            {manualEntryUser != null && (
-              <ManualEntryUserContent user={manualEntryUser} />
-            )}
-          </CardContent>
-        </Card>
-      </Vertical>
-    </>
+        <CardContent>
+          {manualEntryUser != null && (
+            <ManualEntryUserContent user={manualEntryUser} />
+          )}
+        </CardContent>
+      </Card>
+      <ScreenshotInstruction />
+    </Vertical>
   );
-  };
+};
+
+const ScreenshotInstruction: React.FC<{}> = () => {
+  return (
+    <Card
+      sx={{
+        color: theme => theme.palette.error.contrastText,
+        bgcolor: "error.light",
+      }}
+    >
+      <CardContent>
+        <Typography variant="body1">
+          Please take a screenshot of this screen and show it at the
+          registration desk along with a valid ID to collect your wrist band.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 const LabelledText: React.FC<{
   label: string;
-  value: string
+  value: string;
 }> = ({ label, value }) => {
   if (isEmpty(value)) {
-    return null
+    return null;
   }
-    return (
-      <Typography variant="body1">
-        <strong>{label}: </strong>
-        {value}
-      </Typography>
-    );
-  }
+  return (
+    <Typography variant="body1">
+      <strong>{label}: </strong>
+      {value}
+    </Typography>
+  );
+};
 
 const ManualEntryUserContent: React.FC<{
   user: ManualEntryUser;
-}> = ({user}) => {
+}> = ({ user }) => {
   return (
     <Vertical>
       <LabelledText label="Name" value={user.name} />
