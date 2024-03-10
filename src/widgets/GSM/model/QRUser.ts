cc3161ab63b7163c32f274992b4765e92b43b7a7
Aakash Shah <appsparkler@gmail.com>
2024-03-10 15:12:53 +0530

    chore: add QRUser

---
+++ src/widgets/GSM/model/QRUser.ts
@@ -0,0 +1,10 @@
+interface IQRUser {
+  name: string;
+  eventName: string;
+  sessionName: string;
+  registrationId: string;
+  pnr: string;
+  checkinTime: number;
+  uid: string | null;
+  platform: "WEB";
+}
