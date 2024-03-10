11d38313810e226eca6ed1231102f8546732221d
Aakash Shah <appsparkler@gmail.com>
2024-03-10 19:52:08 +0530

    chore: moved isValidUser to separate module

--- src/widgets/GSM/features/HomeScreen/HomeScreen.tsx
+++ src/widgets/GSM/features/HomeScreen/HomeScreen.tsx
@@ -11,9 +11,9 @@ import {
   Typography,
 } from "@mui/material";
 import { Horizontal, Vertical } from "components";
-import { isEmpty } from "lodash/fp";
 import React, { ChangeEvent } from "react";
 import { ManualEntryUser } from "widgets/GSM/model/ManualEntryUser";
+import { isValidUser } from "./utils/isValidUser";
 
 const maxWidth = 420;
 
@@ -160,38 +160,3 @@ export const HomeScreen: React.FC<{
     </Vertical>
   );
 };
-
-function nameIsNotEmpty(user: ManualEntryUser): boolean {
-  return !isEmpty(user.name);
-}
-
-function mobileIsValid(mobileNo: string): boolean {
-  if (isEmpty(mobileNo)) return true;
-
-  const regex = new RegExp("^\\+[1-9]\\d{5,14}$");
-  return mobileNo.match(regex) !== null;
-}
-
-function emailIsValid(email: string): boolean {
-  if (isEmpty(email)) return true;
-  const emailRegex = new RegExp("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
-  return email.match(emailRegex) !== null;
-}
-
-function mobileOrEmailExistsAndAreValid(user: ManualEntryUser): boolean {
-  const mobileOrEmailExists = !isEmpty(user.mobileNo) || !isEmpty(user.email);
-  return (
-    mobileOrEmailExists &&
-    mobileIsValid(user.mobileNo) &&
-    emailIsValid(user.email)
-  );
-}
-
-// function mobileOrEmailExistsAndAreValid(mobileNo: string, email: string): boolean {
-
-// }
-
-function isValidUser(user: ManualEntryUser): boolean {
-  if (mobileOrEmailExistsAndAreValid(user) && nameIsNotEmpty(user)) return true;
-  return false;
-}
