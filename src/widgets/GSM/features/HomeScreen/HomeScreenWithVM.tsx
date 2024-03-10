90a291248f8e7d1eb507266899bbc6e22766cc2a
Aakash Shah <appsparkler@gmail.com>
2024-03-10 16:08:28 +0530

    chore: wip

--- src/widgets/GSM/features/HomeScreen/HomeScreenWithVM.tsx
+++ src/widgets/GSM/features/HomeScreen/HomeScreenWithVM.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HomeScreen } from "./HomeScreen";
 import { ManualEntryUser } from "widgets/GSM/model/ManualEntryUser";
 import { useAppDispatch, useAppSelector } from "../redux-app/hooks";
+import { homeScreenActions } from "./homeScreenSlice";
 
 export const HomeScreenWithVM: React.FC<{}> = () => {
   const state = useAppSelector((state) => state.homeScreen);
 
   const dispatch = useAppDispatch();
+
+  function handleChangeUserDetails(user: ManualEntryUser): void {
+    dispatch(homeScreenActions.updateUserInfo(user));
+  }
+
+  useEffect(() => {
+    dispatch(homeScreenActions.resetUserInfo());
+  }, [dispatch]);
 
   return (
     <HomeScreen
@@ -22,9 +31,7 @@ export const HomeScreenWithVM: React.FC<{}> = () => {
       onClickScan={function (): void {
         throw new Error("Function not implemented.");
       }}
-      onChangeUserDetails={function (user: ManualEntryUser): void {
-        throw new Error("Function not implemented.");
-      }}
+      onChangeUserDetails={handleChangeUserDetails}
     />
   );
 };
