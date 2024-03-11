52005bdd80be4af2ff653ea67f2a08014271e969
Aakash Shah <appsparkler@gmail.com>
2024-03-11 09:42:25 +0530

    chore: wip

--- src/widgets/GSM/features/router/successScreenRoute.tsx
+++ src/widgets/GSM/features/router/successScreenRoute.tsx
@@ -1,6 +1,23 @@
-import { RouteObject, useNavigate } from "react-router-dom";
+import {
+  LoaderFunction,
+  RouteObject,
+  redirect,
+  useNavigate,
+} from "react-router-dom";
 import { SuccessScreenWithVM } from "../SuccessScreen/SuccessScreenWithVM";
 import { AppRoutes } from "./AppRoutes";
+import store from "../redux-app/store";
+
+const loader: LoaderFunction = () => {
+  const state = store.getState();
+  if (
+    state.successScreen.manualEntryUser === null &&
+    state.successScreen.qrUser === null
+  ) {
+    return redirect(AppRoutes.HOME_SCREEN);
+  }
+  return null;
+};
 
 const SuccessScreenComponent: React.FC<{}> = () => {
   const navigate = useNavigate();
@@ -15,4 +32,5 @@ const SuccessScreenComponent: React.FC<{}> = () => {
 export const successScreenRoute: RouteObject = {
   path: AppRoutes.SUCCESS_SCREEN,
   Component: SuccessScreenComponent,
+  loader,
 };
