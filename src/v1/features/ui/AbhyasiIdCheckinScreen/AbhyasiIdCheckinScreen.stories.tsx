6fd3b9b0be89acb9dec032ed00f49fb2b4c95089
Aakash Shah <appsparkler@gmail.com>
2024-04-11 09:53:08 +0530

    chore: wip

---
+++ src/v1/features/ui/AbhyasiIdCheckinScreen/AbhyasiIdCheckinScreen.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { AbhyasiIdCheckinScreen } from "./AbhyasiIdCheckinScreen";
+
+const Story = {
+  component: AbhyasiIdCheckinScreen,
+  title: "features/ui/Abhyasi Id Checkin Screen",
+} as ComponentMeta<typeof AbhyasiIdCheckinScreen>;
+
+const Template: ComponentStory<typeof AbhyasiIdCheckinScreen> = () => (
+  <AbhyasiIdCheckinScreen />
+);
+
+export const AbhyasiIdCheckinScreenStory: ComponentStory<
+  typeof AbhyasiIdCheckinScreen
+> = Template.bind({});
+AbhyasiIdCheckinScreenStory.args = {};
+AbhyasiIdCheckinScreenStory.storyName = "Abhyasi Id Checkin Screen";
+
+export default Story;
---
+++ src/v1/features/ui/AbhyasiIdCheckinScreen/AbhyasiIdCheckinScreen.tsx
@@ -0,0 +1,3 @@
+export const AbhyasiIdCheckinScreen = () => {
+  return <>AbhyasiIdCheckinScreen</>;
+};
