c2225ea3f2106bdaebe65965d4f3b8cbac1ae4d2
Aakash Shah <appsparkler@gmail.com>
2024-04-11 08:49:57 +0530

    chore: checkin success screen

--- src/v1/features/ui/SuccessScreen/SuccessScreen.tsx
+++ src/v1/features/ui/SuccessScreen/SuccessScreen.tsx
@@ -1,3 +1,33 @@
+import { CheckCircle } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography/Typography";
+import { Vertical } from "components";
+
 export const SuccessScreen = () => {
-  return <>Success Screen</>;
+  return (
+    <Vertical
+      alignItems={"center"}
+      justifyContent={"center"}
+      maxWidth={400}
+      sx={{
+        width: "auto",
+        mx: "auto",
+        p: 2,
+      }}
+      gap={2}
+    >
+      <Vertical mt={12} justifyContent={"center"} alignItems={"center"}>
+        <CheckCircle
+          color="success"
+          sx={{
+            fontSize: 80,
+          }}
+        />
+        <Typography variant="h5">Checked In</Typography>
+      </Vertical>
+      <Button type="button" variant="contained" size="large" disableElevation>
+        Return to Main Screen
+      </Button>
+    </Vertical>
+  );
 };
