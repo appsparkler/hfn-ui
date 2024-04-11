f928250461f8589f973ae4bd6a93e0103161d655
Aakash Shah <appsparkler@gmail.com>
2024-04-11 14:00:48 +0530

    chore: wip

--- src/v1/features/ui/EmailOrMobileCheckinScreen/EmailOrMobileCheckinScreen.tsx
+++ src/v1/features/ui/EmailOrMobileCheckinScreen/EmailOrMobileCheckinScreen.tsx
@@ -1,8 +1,23 @@
-import { ScreenWrapper } from "../components/ScreenWrapper/ScreenWrapper"
+import { TextField } from "@mui/material";
+import { BatchSelectField } from "../components/BatchSelectField/BatchSelectField";
+import { CardWithHeader } from "../components/CardWithHeader/CardWithHeader";
+import { ScreenWrapper } from "../components/ScreenWrapper/ScreenWrapper";
+import { Vertical } from "components";
 
 export const EmailOrMobileCheckinScreen = () => {
-
-    return <ScreenWrapper>
-        
+  return (
+    <ScreenWrapper>
+      <CardWithHeader heading="Email Or Mobile Checkin">
+        <Vertical gap={1}>
+          <BatchSelectField defaultValue="batch-1" onChange={() => {}} />
+          <TextField
+            type="text"
+            variant="standard"
+            label={"Full Name"}
+            fullWidth
+          />
+        </Vertical>
+      </CardWithHeader>
     </ScreenWrapper>
-}
+  );
+};
