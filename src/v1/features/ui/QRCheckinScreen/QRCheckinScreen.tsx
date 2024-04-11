7add4d12c75f1cdf31de2046ca306b3c42ba4d8b
Aakash Shah <appsparkler@gmail.com>
2024-04-11 16:32:53 +0530

    chore: wip

--- src/v1/features/ui/QRCheckinScreen/QRCheckinScreen.tsx
+++ src/v1/features/ui/QRCheckinScreen/QRCheckinScreen.tsx
@@ -1,9 +1,22 @@
+import { CardWithClickableHeader } from "../components/CardWithHeader/CardWithHeader";
 import { ScreenWrapper } from "../components/ScreenWrapper/ScreenWrapper";
+
+const QRCheckinCard: React.FC<{}> = () => {
+  return (
+    <CardWithClickableHeader
+      onClickHeader={() => {}}
+      heading="Ravi Balla"
+      isSelected={true}
+    >
+      The qr details
+    </CardWithClickableHeader>
+  );
+};
 
 export const QRCheckinScreen: React.FC<{}> = () => {
   return (
     <ScreenWrapper>
-      <div>QR Checkin Screen</div>
+      <QRCheckinCard />
     </ScreenWrapper>
   );
 };
--- src/v1/features/ui/components/CardWithHeader/CardWithHeader.tsx
+++ src/v1/features/ui/components/CardWithHeader/CardWithHeader.tsx
@@ -1,3 +1,4 @@
+import { CardActionArea } from "@mui/material";
 import Card from "@mui/material/Card/Card";
 import CardContent from "@mui/material/CardContent/CardContent";
 import CardMedia from "@mui/material/CardMedia/CardMedia";
@@ -19,6 +20,31 @@ export const CardWithHeader: React.FC<{
           {heading}
         </Typography>
       </CardMedia>
+      <CardContent>{children}</CardContent>
+    </Card>
+  );
+};
+
+export const CardWithClickableHeader: React.FC<{
+  heading: string;
+  isSelected: boolean;
+  onClickHeader: () => void;
+  children: React.ReactNode;
+}> = ({ heading, isSelected, onClickHeader, children }) => {
+  return (
+    <Card elevation={1}>
+      <CardActionArea onClick={onClickHeader}>
+        <CardMedia>
+          <Typography
+            p={2}
+            bgcolor={isSelected ? "primary.main" : "primary.light"}
+            color={"primary.contrastText"}
+            variant="h5"
+          >
+            {heading}
+          </Typography>
+        </CardMedia>
+      </CardActionArea>
       <CardContent>{children}</CardContent>
     </Card>
   );
