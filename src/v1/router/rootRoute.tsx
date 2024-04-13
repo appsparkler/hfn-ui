02fa84cb4f568397d20c7658f6a77f111e38194c
Aakash Shah <appsparkler@gmail.com>
2024-04-13 14:08:26 +0530

    chore: wip

--- src/v1/router/rootRoute.tsx
+++ src/v1/router/rootRoute.tsx
@@ -2,10 +2,15 @@ import { Outlet, RouteObject } from "react-router-dom";
 import { mainScreenRouteObject } from "./mainScreenRoute";
 import { abhyasiIdCheckinRouteObject } from "./abhyasiIdCheckinRoute";
 import { ErrorElement } from "./errorRoute";
+import { successScreenRoute } from "./successScreenRoute";
 
 export const rootRoute: RouteObject = {
   path: "/",
   ErrorBoundary: ErrorElement,
   Component: () => <Outlet />,
-  children: [mainScreenRouteObject, abhyasiIdCheckinRouteObject],
+  children: [
+    mainScreenRouteObject,
+    abhyasiIdCheckinRouteObject,
+    successScreenRoute,
+  ],
 };
