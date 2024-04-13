6d55bff4b2bfb1acbb0d67655907f23dd4cfb658
Aakash Shah <appsparkler@gmail.com>
2024-04-13 13:42:17 +0530

    chore: wip

--- src/v1/router/abhyasiIdCheckinRoute/index.tsx
+++ src/v1/router/abhyasiIdCheckinRoute/index.tsx
@@ -1,14 +1,28 @@
-import { LoaderFunction, RouteObject, useLocation } from "react-router-dom";
+import {
+  LoaderFunction,
+  RouteObject,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { appRoutes } from "../../model/data/routes";
 import { ILocationState } from "v1/model/interfaces/ILocationState";
 import { AbhyasiIdCheckinScreenConnected } from "v1/features/AbhyasiIdCheckinScreen/AbhyasiIdCheckinScreenConnected";
 
 const Component = () => {
   const { state } = useLocation() as { state: ILocationState };
+  const navigate = useNavigate();
+  const handleCancel = () => {
+    navigate(appRoutes.MAIN_SCREEN);
+  };
+  const handleCheckin = () => {
+    navigate(appRoutes.SUCCESS_SCREEN);
+  };
   return (
     <AbhyasiIdCheckinScreenConnected
       abhyasiId={state.inputValue}
       batchInitialValue={state.batch}
+      onClickCancel={handleCancel}
+      onCheckin={handleCheckin}
     />
   );
 };
